f8da1b99893a3b7dd13d3de7562e8fd29c3bb6d7
Prashant Kadam <kadam.prashant1993@gmail.com>
2017-12-18 15:38:09 +0530

    Unclose connection

--- src/main/java/com/org/App.java
+++ src/main/java/com/org/App.java
@@ -1,5 +1,11 @@
 package com.org;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
@@ -9,8 +15,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.org.model.JobBean;
 
 public class App {
-	public static void main(String[] args) {
-adas
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/xe", "admin", "pass@123");
+		Statement createStatement = connection.createStatement();
+		ResultSet resultSet = createStatement.executeQuery("select * from T_PMS_STATUS");
+		while (resultSet.next()) {
+			System.out.println(resultSet.getString("PMS_STATUS"));
+		}
+
 		String[] springConfig  = 
 			{	"spring/batch/config/database.xml", 
 				"spring/batch/config/context.xml",
